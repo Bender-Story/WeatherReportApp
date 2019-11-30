ffb371febea72c3622415158a74925eccbf1bf01
Rahul CH <rahulch@gmail.com>
2019-12-01 01:55:20 +0800

    Update UI Test Cases.

--- app/src/androidTest/java/com/rahul/weatherreportapp/view/searchview/MainActivityTest.kt
+++ app/src/androidTest/java/com/rahul/weatherreportapp/view/searchview/MainActivityTest.kt
@@ -4,7 +4,6 @@ package com.rahul.weatherreportapp.view.searchview
 import android.view.View
 import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
@@ -13,13 +12,21 @@ import androidx.test.runner.AndroidJUnit4
 import com.rahul.weatherreportapp.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import android.widget.EditText
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.BoundedMatcher
+import kotlinx.coroutines.delay
+import org.hamcrest.Matchers.not
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -31,51 +38,14 @@ class MainActivityTest {
 
     @Test
     fun mainActivityTest() {
-
-        onView(withText("Search")).check(matches(isDisplayed()))
-
-
-        val button = onView(
-            allOf(
-                withId(R.id.searchButton),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.searchLayout),
-                        childAtPosition(
-                            IsInstanceOf.instanceOf(android.widget.RelativeLayout::class.java),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        button.check(matches(isDisplayed()))
-
         val textView = onView(
             allOf(
-                withText("Nothing to display, please start typing..."),
-                childAtPosition(
-                    childAtPosition(
-                        IsInstanceOf.instanceOf(android.widget.ScrollView::class.java),
-                        0
-                    ),
-                    2
-                ),
-                isDisplayed()
-            )
-        )
-        textView.check(matches(withText("Nothing to display, please start typing...")))
-
-        val editText = onView(
-            allOf(
-                withId(R.id.searchEditText), withText("Please enter city name."),
+                withText("WeatherReportApp"),
                 childAtPosition(
                     allOf(
-                        withId(R.id.searchLayout),
+                        withId(R.id.action_bar),
                         childAtPosition(
-                            IsInstanceOf.instanceOf(android.widget.RelativeLayout::class.java),
+                            withId(R.id.action_bar_container),
                             0
                         )
                     ),
@@ -84,7 +54,54 @@ class MainActivityTest {
                 isDisplayed()
             )
         )
-        editText.check(matches(withText("Please enter city name.")))
+        textView.check(matches(withText("WeatherReportApp")))
+
+        onView(withId(R.id.searchEditText)).check(matches(withHint("Please enter city name.")))
+        onView(withId(R.id.searchEditText)).check(matches(isDisplayed()))
+
+
+       onView(withText("Nothing to display, please start typing...")).check(matches(isDisplayed()))
+
+        Thread.sleep(500)
+
+        onView(withId(R.id.searchEditText)).perform(clearText(), typeText("London"))
+
+        Thread.sleep(1500)
+
+        onView(withText("Nothing to display, please start typing...")).check(matches(not(isDisplayed())))
+
+        onView(withText("Search Results")).check(matches(isDisplayed()))
+
+        Thread.sleep(1500)
+
+        onView(withId(R.id.searchResultsRecyclerView))
+            .check(matches(atPosition(0, hasDescendant(withText("London - United Kingdom")))))
+
+        onView(withId(R.id.searchResultsRecyclerView))
+            .check(matches(atPosition(1, hasDescendant(withText("London - Canada")))))
+
+
+
+
+
+
+
+    }
+
+    private fun atPosition(position: Int, @NonNull itemMatcher: Matcher<View>): Matcher<View> {
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+        }
     }
 
     private fun childAtPosition(
